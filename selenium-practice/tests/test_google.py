76df7cec55ad6dd2e7aeba83d0af4b2283c58d8e
Christine Miller <apriljmiller4592@gmail.com>
2026-08-03 21:44:22 -0700

    Testing the Internet for Selenium practice

--- selenium-practice/tests/test_google.py
+++ selenium-practice/tests/test_google.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 def test_open_google():
 
@@ -10,10 +12,12 @@ def test_open_google():
 
     try:
         # Act
-        driver.get("https://www.google.com")
+        driver.get("https://the-internet.herokuapp.com")
+
+        heading = driver.find_element(By.TAG_NAME, "h1")
 
         # Assert
-        assert "Google" in driver.title
+        assert heading.text == "Welcome to the-internet"
 
     finally:
         driver.quit()
